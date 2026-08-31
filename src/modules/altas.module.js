57c1b5f6dea83c32aaceecaf7cb76917aa87df6b
pablobianchino <pablobianchino@gmail.com>
2026-08-31 13:29:08 -0300

    deploy: v5.9.14 - Actualizar nombre de app PWA a Mandala Admision

--- src/modules/altas.module.js
+++ src/modules/altas.module.js
@@ -21,7 +21,7 @@ import {
     sincronizarEventoAltaConfirmadaCalendar, 
     eliminarEventoAltaSeguro,
     validarConflictoCalendarEnVivo
-} from "../services/calendar.service.js?v=5.9.13";
+} from "../services/calendar.service.js?v=5.9.14";
 import { calcularProximaFechaDiaHora } from "./match.module.js";
 
 const mapaDiasCodigos = { 'D': 'Domingo', 'L': 'Lunes', 'M': 'Martes', 'X': 'Miércoles', 'J': 'Jueves', 'V': 'Viernes', 'S': 'Sábado' };
