c81f67ecfff273ec8a828b546e62db0182071e7f
pablobianchino <pablobianchino@gmail.com>
2026-08-31 17:23:46 -0300

    deploy: v5.9.18 - Anti-flicker de login en moviles, apertura robusta de modal y listener global on focus

--- src/modules/altas.module.js
+++ src/modules/altas.module.js
@@ -21,7 +21,7 @@ import {
     sincronizarEventoAltaConfirmadaCalendar, 
     eliminarEventoAltaSeguro,
     validarConflictoCalendarEnVivo
-} from "../services/calendar.service.js?v=5.9.17";
+} from "../services/calendar.service.js?v=5.9.18";
 import { calcularProximaFechaDiaHora } from "./match.module.js";
 
 const mapaDiasCodigos = { 'D': 'Domingo', 'L': 'Lunes', 'M': 'Martes', 'X': 'Miércoles', 'J': 'Jueves', 'V': 'Viernes', 'S': 'Sábado' };
