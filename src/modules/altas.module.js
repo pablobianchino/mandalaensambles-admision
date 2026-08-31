d539e954a8ddf011dd72e6938e7e663ea85619eb
pablobianchino <pablobianchino@gmail.com>
2026-08-31 13:10:13 -0300

    deploy: v5.9.13 - Notificaciones 09hs Admisor y Evaluador con boton unico Ver Ficha

--- src/modules/altas.module.js
+++ src/modules/altas.module.js
@@ -21,7 +21,7 @@ import {
     sincronizarEventoAltaConfirmadaCalendar, 
     eliminarEventoAltaSeguro,
     validarConflictoCalendarEnVivo
-} from "../services/calendar.service.js?v=5.9.12";
+} from "../services/calendar.service.js?v=5.9.13";
 import { calcularProximaFechaDiaHora } from "./match.module.js";
 
 const mapaDiasCodigos = { 'D': 'Domingo', 'L': 'Lunes', 'M': 'Martes', 'X': 'Miércoles', 'J': 'Jueves', 'V': 'Viernes', 'S': 'Sábado' };
