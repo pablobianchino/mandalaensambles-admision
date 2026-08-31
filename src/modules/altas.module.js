213c3455e813a1773014237495342ebc59a43d96
pablobianchino <pablobianchino@gmail.com>
2026-08-31 11:27:57 -0300

    deploy: v5.9.12 - Test notificaciones push PWA con opcion inmediata y fallback DOM robusto

--- src/modules/altas.module.js
+++ src/modules/altas.module.js
@@ -21,7 +21,7 @@ import {
     sincronizarEventoAltaConfirmadaCalendar, 
     eliminarEventoAltaSeguro,
     validarConflictoCalendarEnVivo
-} from "../services/calendar.service.js?v=5.9.10";
+} from "../services/calendar.service.js?v=5.9.12";
 import { calcularProximaFechaDiaHora } from "./match.module.js";
 
 const mapaDiasCodigos = { 'D': 'Domingo', 'L': 'Lunes', 'M': 'Martes', 'X': 'Miércoles', 'J': 'Jueves', 'V': 'Viernes', 'S': 'Sábado' };
